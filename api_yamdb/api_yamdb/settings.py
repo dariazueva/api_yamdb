f6faaa73254a4b641ff08e419ba56f5cad5c34d8
Andrew Kirov <AndrewKirov@me.com>
2023-09-26 17:33:46 +0500

    rename app

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -24,7 +24,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'django_filters',
     'api',
-    'compositions',
+    'reviews',
 ]
 
 MIDDLEWARE = [
--- api_yamdb/reviews/apps.py
+++ api_yamdb/reviews/apps.py
@@ -3,4 +3,5 @@ from django.apps import AppConfig
 
 class CompositionsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'compositions'
+    name = 'reviews'
+    verbose_name = ''
