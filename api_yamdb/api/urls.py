1282a92b6a97e1dec8d4b0df1f2b93cb215b1eee
Georgii Medko <georgii.medko@gmail.com>
2023-10-08 22:52:02 +0500

    edit reviews, comment viewset v1.4

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -3,14 +3,34 @@ from rest_framework.routers import DefaultRouter
 
 from api.views import (CustomUserViewSet, TokenObtainView,
                        UserRegistrationViewSet)
-from .views import (CategoryViewSet, GenreViewSet, TitlesViewSet)
+from .views import (CategoryViewSet, GenreViewSet, TitlesViewSet, ReviewViewSet, CommentViewSet)
 
 router_version1 = DefaultRouter()
-router_version1.register('users', CustomUserViewSet, basename='user')
-router_version1.register(r'categories', CategoryViewSet)
-router_version1.register(r'genres', GenreViewSet)
-router_version1.register(r'titles', TitlesViewSet)
-
+router_version1.register(
+    'users',
+    CustomUserViewSet,
+    basename='user')
+router_version1.register(
+    r'categories',
+    CategoryViewSet)
+router_version1.register(
+    r'genres',
+    GenreViewSet)
+router_version1.register(
+    'titles',
+    TitlesViewSet,
+    basename='titles'
+)
+router_version1.register(
+    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+    CommentViewSet,
+    basename='comments'
+)
+router_version1.register(
+    r'titles/(?P<title_id>\d+)/reviews',
+    ReviewViewSet,
+    basename='reviews'
+)
 
 urlpatterns = [
     path('', include(router_version1.urls)),
@@ -21,4 +41,4 @@ urlpatterns = [
         ),
         name='signup'
     ),
-    path('auth/token/', TokenObtainView.as_view(), name='token_obtain'),]
+    path('auth/token/', TokenObtainView.as_view(), name='token_obtain'), ]
